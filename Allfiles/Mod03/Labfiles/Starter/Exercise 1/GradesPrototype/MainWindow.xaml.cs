28621840f0989c214626e4496e6d3279759ec846
Daveena Cooper <daveena.cooper@outlook.com>
2020-03-28 22:49:34 -0700

    Adding changes for MOD3 Ex1

--- Allfiles/Mod03/Labfiles/Starter/Exercise 1/GradesPrototype/MainWindow.xaml.cs
+++ Allfiles/Mod03/Labfiles/Starter/Exercise 1/GradesPrototype/MainWindow.xaml.cs
@@ -34,19 +34,32 @@ namespace GradesPrototype
         // TODO: Exercise 1: Task 3a: Display the logon view and and hide the list of students and single student view
         public void GotoLogon()
         {
-
+            // Display the logon view and hide the list of students and single student view
+            logonPage.Visibility = Visibility.Visible;
+            studentsPage.Visibility = Visibility.Collapsed;
+            studentProfile.Visibility = Visibility.Collapsed;
         }
 
         // TODO: Exercise 1: Task 4c: Display the list of students
         private void GotoStudentsPage()
-        {            
+        {
+            // Hide the view for a single student (if it is visible)
+            studentProfile.Visibility = Visibility.Collapsed;
 
+            // Display the list of students
+            studentsPage.Visibility = Visibility.Visible;
+            studentsPage.Refresh();
         }
 
         // TODO: Exercise 1: Task 4b: Display the details for a single student
         public void GotoStudentProfile()
         {
+            // Hide the list of students
+            studentsPage.Visibility = Visibility.Collapsed;
 
+            // Display the view for a single student
+            studentProfile.Visibility = Visibility.Visible;
+            studentProfile.Refresh();
         }
         #endregion
 
@@ -54,7 +67,14 @@ namespace GradesPrototype
 
         // TODO: Exercise 1: Task 3b: Handle successful logon
         // Update the display and show the data for the logged on user
-
+        // Handle successful logon
+        private void Logon_Success(object sender, EventArgs e)
+        {
+            // Update the display and show the data for the logged on user
+            logonPage.Visibility = Visibility.Collapsed;
+            gridLoggedIn.Visibility = Visibility.Visible;
+            Refresh();
+        }
         // Handle logoff
         private void Logoff_Click(object sender, RoutedEventArgs e)
         {
@@ -75,7 +95,8 @@ namespace GradesPrototype
         // Set the global context to the name of the student and call the GotoStudentProfile method to display the details of the student
         private void studentsPage_StudentSelected(object sender, StudentEventArgs e)
         {
-
+            SessionContext.CurrentStudent = e.Child;
+            GotoStudentProfile();
         }
         #endregion
 
@@ -84,7 +105,24 @@ namespace GradesPrototype
         // TODO: Exercise 1: Task 4a: Update the display for the logged on user (student or teacher)
         private void Refresh()
         {
- 
+            switch (SessionContext.UserRole)
+            {
+                case Role.Student:
+                    // Display the student name in the banner at the top of the page
+                    txtName.Text = string.Format("Welcome {0}", SessionContext.UserName);
+
+                    // Display the details for the current student
+                    GotoStudentProfile();
+                    break;
+
+                case Role.Teacher:
+                    // Display the teacher name in the banner at the top of the page
+                    txtName.Text = string.Format("Welcome {0}", SessionContext.UserName);
+
+                    // Display the list of students for the teacher
+                    GotoStudentsPage();
+                    break;
+            }
         }
         #endregion
     }
